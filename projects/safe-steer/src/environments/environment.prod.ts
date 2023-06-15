0933ddd08703d1d96d852e4d7b7d294b3f421f3b
coriumalpha <toroidalmirror@gmail.com>
2023-06-15 09:50:38 +0200

    Add environment

---
+++ projects/safe-steer/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    //TODO: Replace with production API URL
+    api: 'https://localhost:3000'
+};
---
+++ projects/safe-steer/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    api: 'https://localhost:3000'
+};
