ca83bb8e33a94282e76d8e4366d8d845b5619ba1
coriumalpha <toroidalmirror@gmail.com>
2023-06-18 17:02:06 +0200

    Update API URL to point to Azure App Service with JSON Server

--- projects/safe-steer/src/environments/environment.prod.ts
+++ projects/safe-steer/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     //TODO: Replace with production API URL
-    api: 'polite-mushroom-0c65cab10.3.azurestaticapps.net'
+    api: 'steersafeapiserver.azurewebsites.net/',
+    baseUrl: 'polite-mushroom-0c65cab10.3.azurestaticapps.net'
 };
