a9ddf474a140f485a3421387c74889880a01543e
coriumalpha <toroidalmirror@gmail.com>
2023-06-19 10:42:27 +0200

    Refactor Learning Path service API requests

--- projects/safe-steer/src/app/services/learning-path.service.ts
+++ projects/safe-steer/src/app/services/learning-path.service.ts
@@ -1,24 +1,26 @@
-
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { filter, switchMap, tap } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 import { LearningPathStore } from '../stores/learning-path.store';
 import { LearningPath } from '../models/learning-path.model';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class LearningPathService {
+  private api = environment.api;
+
   constructor(private learningPathStore: LearningPathStore, private http: HttpClient) {}
 
   get() {
-    return this.http.get<LearningPath[]>('http://localhost:3000/learningPaths')
+    return this.http.get<LearningPath[]>(`${this.api}/learningPaths`)
       .pipe(tap(entities => {
         this.learningPathStore.set(entities);
       }));
   }
 
   getLearningPath(id: number) {
-    return this.http.get<LearningPath>(`http://localhost:3000/learningPaths/${id}`)
+    return this.http.get<LearningPath>(`${this.api}/learningPaths/${id}`)
       .pipe(tap(entity => {
         this.learningPathStore.add(entity);
       }));
@@ -26,7 +28,7 @@ export class LearningPathService {
 
   addSkillToPath(learningPath: LearningPath, skillId: number): Observable<LearningPath> | Observable<never> {
     const updatedLearningPath = { ...learningPath, skills: [...learningPath.skills, skillId] };
-    return this.http.put<LearningPath>(`http://localhost:3000/learningPaths/${learningPath.id}`, updatedLearningPath)
+    return this.http.put<LearningPath>(`${this.api}/learningPaths/${learningPath.id}`, updatedLearningPath)
       .pipe(tap(() => {
         this.learningPathStore.update(learningPath.id, updatedLearningPath);
       }));
@@ -35,8 +37,7 @@ export class LearningPathService {
   removeSkillFromPath(learningPath: LearningPath, skillId: number): Observable<LearningPath> | Observable<never> {
     const updatedSkills = learningPath.skills.filter((id) => id !== skillId);
     const updatedLearningPath = { ...learningPath, skills: updatedSkills };
-    return this.http
-      .put<LearningPath>(`http://localhost:3000/learningPaths/${learningPath.id}`, updatedLearningPath)
+    return this.http.put<LearningPath>(`${this.api}/learningPaths/${learningPath.id}`, updatedLearningPath)
       .pipe(
         tap(() => {
           this.learningPathStore.update(learningPath.id, updatedLearningPath);
