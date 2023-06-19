d6e1b4e64ee8094405fa711edc4368fe8fa97de3
coriumalpha <toroidalmirror@gmail.com>
2023-06-20 00:13:48 +0200

    Add category selection Unit Test

--- projects/safe-steer/src/app/pages/demo/demo.component.spec.ts
+++ projects/safe-steer/src/app/pages/demo/demo.component.spec.ts
@@ -13,6 +13,9 @@ import { Skill } from '../../models/skill.model';
 import { LearningPath } from '../../models/learning-path.model';
 import { ComponentsModule } from '../../components/components.module';
 import { SkillDialogComponent } from '../../components/skill-dialog/skill-dialog.component';
+import { SkillStore } from '../../stores/skill.store';
+import { SkillQuery } from '../../querys/skill.query';
+import { Category } from '../../models/category.model';
 
 class SkillServiceStub {
   get() { return of(testSkills); }
@@ -84,61 +87,61 @@ describe('DemoComponent', () => {
     const skill: Skill = testSkills[0];
     const learningPathService = TestBed.inject(LearningPathService);
     const spy = spyOn(learningPathService, 'addSkillToPath').and.returnValue(of(testLearningPaths[0]));
-  
+
     component.learningPath$ = of(testLearningPaths[0]);
-  
+
     component.addToLearningPath(skill);
-  
+
     tick();
-  
+
     expect(spy).toHaveBeenCalled();
   }));
-  
+
   it('should remove skill from learning path when removeFromLearningPath() is called', fakeAsync(() => {
     const skill: Skill = testSkills[0];
     const learningPathService = TestBed.inject(LearningPathService);
     const spy = spyOn(learningPathService, 'removeSkillFromPath').and.returnValue(of(testLearningPaths[0]));
-  
+
     component.learningPath$ = of(testLearningPaths[0]);
-  
+
     component.removeFromLearningPath(skill);
-  
+
     tick();
-  
+
     expect(spy).toHaveBeenCalled();
   }));
 
   it('should return true if skill is in learning path', () => {
     const skill: Skill = testSkills[0];
-    
+
     component.learningPath$ = of({
       id: 1,
       skills: [skill.id],
     });
-  
+
     expect(component.skillInLearningPath(skill)).toBeTrue();
   });
-  
+
   it('should return false if skill is not in learning path', () => {
     const skill: Skill = testSkills[0];
-    
+
     component.learningPath$ = of({
       id: 1,
       skills: [],
     });
-  
+
     expect(component.skillInLearningPath(skill)).toBeFalse();
   });
-  
+
   it('should show skill detail dialog when showSkillDetailDialog() is called', () => {
     const skill: Skill = testSkills[0];
     const dialog = TestBed.inject(MatDialog);
     const deleteSubject = new Subject<void>();
-    const dialogRefSpy = { afterClosed : of({}), close: null, componentInstance: { delete: deleteSubject.asObservable() } };
+    const dialogRefSpy = { afterClosed: of({}), close: null, componentInstance: { delete: deleteSubject.asObservable() } };
     spyOn(dialog, 'open').and.returnValue(dialogRefSpy as unknown as MatDialogRef<SkillDialogComponent>);
-  
+
     component.showSkillDetailDialog(skill);
-  
+
     expect(dialog.open).toHaveBeenCalledWith(
       SkillDialogComponent,
       {
@@ -146,6 +149,16 @@ describe('DemoComponent', () => {
         data: skill
       }
     );
+  });
+
+  it('should update selected category when onCategorySelectionChange() is called', () => {
+    const skillStore = TestBed.inject(SkillStore);
+    const updateSpy = spyOn(skillStore, 'update');
+    const category: Category = testCategories[1];
+  
+    component.onCategorySelectionChange(category);
+  
+    expect(updateSpy).toHaveBeenCalledWith({ ui: { selectedCategory: category } });
   });  
 
 });
